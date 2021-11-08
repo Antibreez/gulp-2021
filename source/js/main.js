c1481307ba8b4febd125ac34cd405ad96073c487
Anton Kiyan <kiyananton@gmail.com>
2021-11-09 00:20:00 +0300

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+// import $ from "jquery";
+import Swiper from 'swiper'
+import getRandomNumber from "./test";
+
+console.log(getRandomNumber());
+console.log('study');
+console.log($('.page__body h1').text());
+
+console.log(Swiper);
+
+const swiper = new Swiper(".swiper");
---
+++ source/js/test.js
@@ -0,0 +1,9 @@
+const getRandomNumber = () => {
+  console.log('hello');
+  console.log('ouch');
+  console.log('haha');
+  console.log('bulllllllll');
+  return Math.random();
+}
+
+export default getRandomNumber
